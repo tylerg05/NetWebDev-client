0cdeca6748678eb83363944a9d0ebe706e63cf1f
tylerg05 <tylergardella05@gmail.com>
2020-09-01 19:02:09 -0500

    assign-2

---
+++ wwwroot/js/balloons.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $('#birthday').pickadate({ format: 'mmmm, d' });
+
+    // uncheck all checkboxes (FireFox)
+    $('.form-check-input').each(function () {
+        $(this).prop('checked', false);
+    });
+
+    // event listener for check/uncheck
+    $('.form-check-input').on('change', function () {
+        // make the image visible
+        $('#' + this.id + 'Img').css('visibility', 'visible')
+        // animate balloon in/out based on checkbox
+        $(this).is(':checked') ?
+            $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown') :
+            $('#' + this.id + 'Img').addClass('animated bounceOutUp');
+    });
+});
